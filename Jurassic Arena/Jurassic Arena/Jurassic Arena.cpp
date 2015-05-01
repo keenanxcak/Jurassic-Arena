4ace1d2235a72e395245c7848b0497c184adc7a1
Sparta24 <jbarta16@my.whitworth.edu>
2015-05-01 14:38:35 -0700

    Poetry

--- Jurassic Arena/Jurassic Arena/Jurassic Arena.cpp
+++ Jurassic Arena/Jurassic Arena/Jurassic Arena.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 using namespace std;
 int main(){
-	cout << "Jon is a tool.";
+	cout << "Jon is a tool.\n";
+	cout << "Keenan's a fool.\n";
+	cout << "and Daniel is cool.\n";
 }
