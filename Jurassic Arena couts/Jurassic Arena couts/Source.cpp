d3636d6f13df238e0e2ac4df0bc4a7f1cb070459
dlee0320 <dlee0320@gmail.com>
2015-05-04 14:57:39 -0700

    cout statements so far

---
+++ Jurassic Arena couts/Jurassic Arena couts/Source.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	// Title Page
+	cout << "                             Welcome to the...\n";
+	cout << endl;
+	cout << "	  ==========================================================\n";
+	cout << "	  |        * *   * ****     **     ****  ****  *****  ***  |\n";
+	cout << "	  |        * *   * *   *   *  *   *     *        *   *     |\n";
+	cout << "	  |        * *   * *   *  *    *   **    **      *   *     |\n";
+	cout << "	  |  *     * *   * ****  ********    *     *     *   *     |\n";
+	cout << "	  |   *   *  *   * *  *  *      *     *     *    *   *     |\n";
+	cout << "	  |    ***    ***  *   * *      * ****  ****   *****  ***  |\n";
+	cout << "	  |                                                        |\n";
+	cout << "	  |             **    ****  ***** *    *    **             |\n";
+	cout << "	  |            *  *   *   * *     **   *   *  *            |\n";
+	cout << "	  |           *    *  *   * *     * *  *  *    *           |\n";
+	cout << "	  |          ******** ****  ***** *  * * ********          |\n";
+	cout << "	  |          *      * *  *  *     *   ** *      *          |\n";
+	cout << "	  |          *      * *   * ***** *    * *      *          |\n";
+	cout << "	  ==========================================================\n";
+	cout << endl;
+	cout << "Press enter to continue.";
+	char c1;
+	cin.get(c1);
+	cout << endl;
+
+	// Introduction to the game
+	cout << "In the Jurassic Arena, two challengers enter and only one leaves victorious.\n";
+	return 0;
+}
